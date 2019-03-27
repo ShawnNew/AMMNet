d16ed74079f85b1506755867d3a04619350ba769
ShawnNew <ncxinhanzhong@gmail.com>
2019-03-27 11:53:11 -0400

    make 3 conv layer content loss

--- model/loss.py
+++ model/loss.py
@@ -35,19 +35,15 @@ def cross_entropy2d(input, target, weight=None, size_average=True):
 class VGG16ContentModel(nn.Module):
     def __init__(self, **kwargs):
         super(VGG16ContentModel, self).__init__()
-        idx_feature_map = kwargs['feature_map']
         pretrained_model = getattr(models, kwargs['model'])(pretrained=True).features
+        self.weights = kwargs['weights']
         # get certain layers to construct content-loss module
-        #model = pretrained_model[:idx_feature_map+1] # new content Sequential module network
-        self.conv1= pretrained_model[:1]
-        self.conv2 = pretrained_model[1:3]
-        self.conv3 = pretrained_model[3:6]
-        self.conv4 = pretrained_model[6:8]
-        #self.model = model
+        self.conv1= pretrained_model[:2]  # relu 1
+        self.conv2 = pretrained_model[2:5] # maxpool 1
+        self.conv3 = pretrained_model[5:7]
+        self.conv4 = pretrained_model[7:10] # maxpool 2
 
     def forward(self, pred, gt):
-        #content_pred = self.model(pred)
-        #content_gt = self.model(gt)
         content_pred_conv1 = self.conv1(pred)
         content_gt_conv1 = self.conv1(gt)
         content_pred_conv2 = self.conv2(pred)
@@ -56,7 +52,8 @@ class VGG16ContentModel(nn.Module):
         content_gt_conv3 = self.conv3(gt)
         content_pred_conv4 = self.conv4(pred)
         content_gt_conv4 = self.conv4(gt)
-        loss = F.mse_loss(content_pred_conv1, content_gt_conv1)+F.mse_loss(content_pred_conv2, content_gt_conv2)+ \
-               F.mse_loss(content_pred_conv3, content_gt_conv3)+F.mse_loss(content_pred_conv4, content_gt_conv4)
-        #return F.mse_loss(content_pred, content_gt)
+        loss = self.weights[0] * F.mse_loss(content_pred_conv1, content_gt_conv1) +\
+                self.weights[1] * F.mse_loss(content_pred_conv2, content_gt_conv2) +\
+                self.weights[2] * F.mse_loss(content_pred_conv3, content_gt_conv3) +\
+                self.weights[3] * F.mse_loss(content_pred_conv4, content_gt_conv4)
         return loss
