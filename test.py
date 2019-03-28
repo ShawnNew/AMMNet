8d4716d6f343d44734e448a11cdca70cc902e3ca
ShawnNew <ncxinhanzhong@gmail.com>
2019-03-28 15:30:00 -0400

    ignore large images.

--- test.py
+++ test.py
@@ -7,19 +7,19 @@ import model.loss as module_loss
 import model.metric as module_metric
 import model.model as module_arch
 from train import get_instance
+from torchvision.utils import save_image
 
 
-def main(config, resume):
+def main(config, resume, output_path):
     # setup data_loader instances
     data_loader = getattr(module_data, config['data_loader']['type'])(
         config['data_loader']['args']['data_dir'],
-        batch_size=512,
+        batch_size=1,
         shuffle=False,
         validation_split=0.0,
         training=False,
-        num_workers=2
+        num_workers=1
     )
-
     # build model architecture
     model = get_instance(module_arch, 'arch', config)
     model.summary()
@@ -44,19 +44,28 @@ def main(config, resume):
     total_metrics = torch.zeros(len(metric_fns))
 
     with torch.no_grad():
-        for i, (data, target) in enumerate(tqdm(data_loader)):
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            #
-            # save sample images, or do something with output here
-            #
-            
-            # computing loss, metrics on test set
-            loss = loss_fn(output, target)
-            batch_size = data.shape[0]
-            total_loss += loss.item() * batch_size
-            for i, metric in enumerate(metric_fns):
-                total_metrics[i] += metric(output, target) * batch_size
+        for i, sample_batched in enumerate(tqdm(data_loader)):
+            img_scale1 = sample_batched['image-scale1'].to(device)
+            img_scale2 = sample_batched['image-scale2'].to(device)
+            img_scale3 = sample_batched['image-scale3'].to(device)
+            gt = sample_batched['gt'].to(device)
+            if img_scale1.shape[2] < 1000 and img_scale1.shape[3] < 1000:
+                output = model(img_scale1, img_scale2, img_scale3)
+                filename = os.path.basename(sample_batched['name'][0])
+                filename = os.path.join(output_path, filename)
+                try:
+                    os.stat(output_path)
+                except:
+                    os.mkdir(output_path)
+                # import pdb
+                # pdb.set_trace()
+                save_image(output, filename)
+                # computing loss, metrics on test set
+                loss = loss_fn(output, gt)
+                batch_size = gt.shape[0]
+                total_loss += loss.item() * batch_size
+                for i, metric in enumerate(metric_fns):
+                    total_metrics[i] += metric(output, gt) * batch_size
 
     n_samples = len(data_loader.sampler)
     log = {'loss': total_loss / n_samples}
@@ -65,6 +74,7 @@ def main(config, resume):
 
 
 if __name__ == '__main__':
+    output_path = os.path.join(os.getcwd(), 'test')
     parser = argparse.ArgumentParser(description='PyTorch Template')
 
     parser.add_argument('-r', '--resume', default=None, type=str,
@@ -79,4 +89,4 @@ if __name__ == '__main__':
     if args.device:
         os.environ["CUDA_VISIBLE_DEVICES"]=args.device
 
-    main(config, args.resume)
+    main(config, args.resume, output_path)
